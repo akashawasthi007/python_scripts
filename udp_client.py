e67f33ef22644a61e508f3c6b9c534d3eb279a8d
akashawasthi007 <akashawasthi076>
2021-10-11 14:14:18 +0530

    udp_second_commit

--- udp_client.py
+++ udp_client.py
@@ -1,7 +1,7 @@
 import socket
 
-target_host="www.google.com"
-target_port=80
+target_host="192.168.152.20"
+target_port=9999
 
 client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # create a socket object
 
---
+++ udp_server.py
@@ -0,0 +1,14 @@
+import socket
+import threading
+
+bind_ip="192.168.152.20"
+bind_port=9999
+
+server=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+server.bind((bind_ip,bind_port))
+print("[*] server listening on %s : %d" %(bind_ip,bind_port))
+
+while True:
+    data,addr=server.recvfrom(4096)
+    server.sendto("o yes abhi",addr)
+    print("connection established")
