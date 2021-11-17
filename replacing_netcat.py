e2948b37ca40d8bf7c4ad8b20b154ef2b43b3784
akashawasthi007 <akashawasthi076>
2021-11-17 16:32:54 +0530

    replacing failure

--- replacing_netcat.py
+++ replacing_netcat.py
@@ -26,7 +26,26 @@ def usage():
     print ("echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135")
     sys.exit(0)
 
+def server_loop():
+    global target
+    if not len(target):
+        target="0.0.0.0"
+    server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((target,port))
+    server.listen(5)
 
+    while True:
+        client_socket,add=server.accept()
+        client_thread=threading.Thread(target=client_handler,args=(client_socket,))
+        client_thread.start()
+
+def run_command(command):
+    command=command.rstrip()
+    try:
+        outout=subprocess.check_output(command,stderr=subprocess.STDOUT,shell=True)
+    except:
+        output="Failed to execute command.\n"
+    return output
 
 def client_sender(buffer):
     client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -51,6 +70,44 @@ def client_sender(buffer):
     except:
         print("[*] Exception Handling")
         client.close()
+
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+    if len(upload_destination):
+        file_buffer=""
+        while True:
+            data=client_socket.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer+=data
+        try:
+            file_descriptor=open(upload_destination,"wb")
+            file_descriptor.write(file_buffer)
+            file_descriptor.close()
+            client_socket.send("successfully saved file to %s\r\n" % upload_destination)
+        except:
+            client_socket.send("failed to save the file to %s\r\n" % upload_destination)
+
+    if len(execute):
+        output=run_command(execute)
+        client_socket.send(output)
+    if command:
+        while True:
+            client_socket.send("<BHP:#> ")
+
+            # now we receive until we see a linefeed (enter key)
+            cmd_buffer = b''
+            while b"\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+
+            # we have a valid command so execute it and send back the results
+            response = run_command(cmd_buffer)
+
+            # send back the response
+            client_socket.send(response)
 
 
 def main():
@@ -88,3 +145,4 @@ def main():
     client_sender(buffer)
     if listen:
         server_loop()
+main()
